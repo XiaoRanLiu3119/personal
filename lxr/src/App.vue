a81b5d14e667d84392972de080df5e112c61940c
XiaoRanLiu3119 <592923276@qq.com>
2020-12-27 14:22:34 +0800

    first-commit

---
+++ lxr/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+	  <div> 测试 </div>
+	  <div> 第二次测试测试 </div>
+	  <div> 第三次测试测试 </div>
+	  <div> 家里测试测试 </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+	  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
